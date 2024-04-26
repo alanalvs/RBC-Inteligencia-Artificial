924661960c21dcfc7522c18491f7eb7ccfd6d40a
Alan Alves <gapapp136@gmail.com>
2024-04-25 22:07:45 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace RBC___Inteligencia_Artificial
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNomeFilme = new System.Windows.Forms.TextBox();
+            this.labelNomeFilme = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNomeFilme
+            // 
+            this.txtNomeFilme.Location = new System.Drawing.Point(115, 72);
+            this.txtNomeFilme.Name = "txtNomeFilme";
+            this.txtNomeFilme.Size = new System.Drawing.Size(222, 20);
+            this.txtNomeFilme.TabIndex = 0;
+            this.txtNomeFilme.Leave += new System.EventHandler(this.txtNomeFilme_Leave);
+            // 
+            // labelNomeFilme
+            // 
+            this.labelNomeFilme.AccessibleName = "";
+            this.labelNomeFilme.AutoSize = true;
+            this.labelNomeFilme.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNomeFilme.Location = new System.Drawing.Point(39, 70);
+            this.labelNomeFilme.Name = "labelNomeFilme";
+            this.labelNomeFilme.Size = new System.Drawing.Size(53, 22);
+            this.labelNomeFilme.TabIndex = 46;
+            this.labelNomeFilme.Text = "Filme";
+            this.labelNomeFilme.Click += new System.EventHandler(this.label1lblCodigoPassagem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(43, 110);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(712, 269);
+            this.dataGridView1.TabIndex = 47;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(791, 425);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.labelNomeFilme);
+            this.Controls.Add(this.txtNomeFilme);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Raciocínio Baseado em Casos";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNomeFilme;
+        private System.Windows.Forms.Label labelNomeFilme;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RBC___Inteligencia_Artificial
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void label1lblCodigoPassagem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNomeFilme_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = "c:/rbc/movies.csv";
+                bool result = File.Exists(path);
+                if (result)
+                {
+                    StreamReader csv = new StreamReader(@"c:/rbc/movies.csv");
+                    string linha;
+                    string[] campo;
+                    while ((linha = csv.ReadLine()) != null)
+                    {
+                        //System.Threading.Thread.Sleep(10000);
+                        campo = linha.Split(';', '0');
+                        dataGridView1.Rows.Add(campo);
+                    }
+
+                    csv.Close();
+                }
+             
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
